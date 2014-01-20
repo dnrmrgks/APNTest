f120ae0a04a78e37d7577d4bcdcc03292bbdfdae
park sung chan <dnrmrgks@naver.com>
2014-01-20 17:51:49 +0900

    푸
    메세

--- APNTest/AppDelegate.m
+++ APNTest/AppDelegate.m
@@ -7,11 +7,17 @@
 //
 
 #import "AppDelegate.h"
-
+#import <AudioToolbox/AudioToolbox.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
+    
+    UIRemoteNotificationType notiType = UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeAlert|UIRemoteNotificationTypeSound;
+    [[UIApplication sharedApplication]registerForRemoteNotificationTypes:notiType];
+  
+
     // Override point for customization after application launch.
     return YES;
 }
@@ -42,5 +48,32 @@
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
+    NSLog(@"Success to register APN:%@",deviceToken);
+}
+-(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
+    NSLog(@"Fail to register APN: %@",[error localizedDescription]);
+}
+
+-(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo{
+    
+    NSLog(@"remote notification:%@" ,[userInfo description]);
+    NSDictionary *apsInfo =[userInfo objectForKey:@"aps"];
+    NSString *alert = [apsInfo objectForKey:@"alert"];
+    NSLog(@"Received Push Alert:%@",alert);
+    NSString *sound= [apsInfo objectForKey:@"sound"];
+    NSLog(@"Received Push Sound:%@",sound);
+    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+    
+    
+    NSString *badge = [apsInfo objectForKey:@"badge"];
+    NSLog(@"Received Push Badge:%@",badge);
+    application.applicationIconBadgeNumber=[[apsInfo objectForKey:@"badge"]integerValue];
+    
+    UIAlertView *alter2 = [[UIAlertView alloc]initWithTitle:@"메시지" message:alert delegate:self cancelButtonTitle:nil otherButtonTitles:@"확인", nil];
+    [alter2 show];
+    
+}
+
 
 @end
